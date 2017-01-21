0c24b292d3ff0a330ead1799115cf4b05a119022
Jay Bosamiya <jaybosamiya@gmail.com>
2017-01-21 12:57:18 +0530

    Use zarith internally

--- boolean.ml
+++ boolean.ml
@@ -80,20 +80,20 @@ end
 
 module F2N ( N : sig val n : int end ) : t_f2n = struct
   let n = N.n
-  type t = int
+  type t = Z.t
 
   exception Not_boolean
 
-  let rec boolstr_to_int chrs : int =
+  let rec boolstr_to_Zt chrs : Z.t =
     match chrs with
-    | [] -> 0
-    | '0' :: xs -> 2 * (boolstr_to_int xs)
-    | '1' :: xs -> 2 * (boolstr_to_int xs) + 1
+    | [] -> Z.zero
+    | '0' :: xs -> Z.mul (Z.of_int 2) (boolstr_to_Zt xs)
+    | '1' :: xs -> Z.add (Z.mul (Z.of_int 2) (boolstr_to_Zt xs)) Z.one
     | _ :: _ -> raise Not_boolean
 
   let parse_boolvec str : t option =
     if String.length str = n
-    then try Some (boolstr_to_int (String.to_list_rev str))
+    then try Some (boolstr_to_Zt (String.to_list_rev str))
       with Not_boolean -> None
     else None
 
@@ -108,7 +108,7 @@ module F2N ( N : sig val n : int end ) : t_f2n = struct
       else None in
     match fans with
     | Some fans ->
-      Some (fun n -> List.nth_exn fans n)
+      Some (fun n -> List.nth_exn fans (Z.to_int n))
     | None ->
       None
 
@@ -116,24 +116,25 @@ module F2N ( N : sig val n : int end ) : t_f2n = struct
     let rec f n b =
       if n = 0
       then []
-      else b mod 2 :: f (n-1) (b/2) in
+      else (Z.rem b (Z.of_int 2)) :: f (n-1) (Z.div b (Z.of_int 2)) in
     List.rev (f n v) |>
-    List.map ~f:Int.to_string |>
+    List.map ~f:Z.to_string |>
     String.concat
 
   let print_func (f:t -> f2) : string =
     List.range 0 (1 lsl n) |>
+    List.map ~f:Z.of_int |>
     List.map ~f |>
     List.map ~f:F2.print |>
     String.concat
 
   let of_f2list xs =
     if List.length xs = n
-    then let rec f xs : int =
+    then let rec f xs : t =
            match xs with
-           | [] -> 0
-           | F2.Zero :: xs -> 2 * (f xs)
-           | F2.One :: xs -> 2 * (f xs) + 1 in
+           | [] -> Z.zero
+           | F2.Zero :: xs -> Z.mul (Z.of_int 2) (f xs)
+           | F2.One :: xs -> Z.add (Z.mul (Z.of_int 2) (f xs)) Z.one in
       Some (f (List.rev xs))
     else None
 
@@ -146,34 +147,38 @@ module F2N ( N : sig val n : int end ) : t_f2n = struct
     let rec f n b =
       if n = 0
       then []
-      else b mod 2 :: f (n-1) (b/2) in
+      else Z.(b mod (of_int 2)) :: f (n-1) Z.(b / (of_int 2)) in
     List.rev (f n v) |>
-    List.map ~f:Int.to_string |>
+    List.map ~f:Z.to_string |>
     List.map ~f:F2.parse |>
     List.map ~f:(fun x -> Option.value_exn x)
 
   module Infix = struct
     let (<&&>) a b : t =
-      a land b
+      Z.(a land b)
 
     let (<++>) a b : t =
-      a lxor b
+      Z.(a lxor b)
 
     let (<..>) a b : F2.t =
-      let rec f a =
-        match a with
-        | 0 -> F2.Zero
-        | 1 -> F2.One
-        | x -> f ((x mod 2) lxor (x / 2)) in
-      f (a <&&> b)
+      let x = a <&&> b in
+      let p = Z.popcount x in
+      if p mod 2 = 0
+      then F2.Zero
+      else F2.One
   end
 
   let all_boolvec () : t list =
-    List.range 0 (1 lsl n)
+    let rec f i : t list =
+      if i = Z.(one lsl n)
+      then []
+      else i :: f Z.(i + one)
+    in
+    f Z.zero
 
   let func_of_f2list xs : (t -> f2) option =
     if List.length xs = 1 lsl n
-    then Some (fun n -> List.nth_exn xs n)
+    then Some (fun n -> List.nth_exn xs (Z.to_int n))
     else None
 
   let func_of_f2list_exn xs : (t -> f2) =
@@ -182,7 +187,7 @@ module F2N ( N : sig val n : int end ) : t_f2n = struct
     | None -> raise (Failure "Wrong length")
 
   let func_to_f2list (f : t -> f2) : f2 list =
-    List.range 0 (1 lsl n) |>
+    all_boolvec () |>
     List.map ~f
 
   let all_func () : (t -> f2) list =
@@ -216,7 +221,7 @@ module F2N ( N : sig val n : int end ) : t_f2n = struct
         let d = List.map2_exn a b ~f:(-) in
         c @ d in
     let wf_list = f f_list in
-    (fun y -> List.nth_exn wf_list y)
+    (fun y -> List.nth_exn wf_list (Z.to_int y))
 
   let walsh_hadamard_transform f : (t -> Z.t) =
     let open F2.Infix in
