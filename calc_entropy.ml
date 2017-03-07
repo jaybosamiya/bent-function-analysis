043662c09a0b74567afe07bfa5cdee089c1b78b9
Jay Bosamiya <jaybosamiya@gmail.com>
2017-03-07 16:40:44 +0530

    allow multiple functions to calculate entropy together

--- calc_entropy.ml
+++ calc_entropy.ml
@@ -3,12 +3,12 @@ open Core_kernel.Std
 open Boolean
 
 let () =
-  let inp = In_channel.(input_line stdin) in
-  match inp with
-  | None ->
+  let inps = In_channel.(input_lines stdin) in
+  match inps with
+  | [] ->
     print_endline "Require boolean function"
-  | Some inp ->
-    let l = String.length inp in
+  | x :: _ ->
+    let l = String.length x in
     let n = Z.(log2up ~$l) in
     let module F2N = F2N ( struct let n = n end ) in
     let hadamard_entropy (f : F2N.t -> f2) : float =
@@ -24,12 +24,14 @@ let () =
           if p = 0.
           then accum
           else accum -. (p *. log p)) /. log 2. in
-    let f = F2N.parse_func inp in
-    match f with
-    | None ->
-      print_endline "Not a valid boolean function";
-      print_string "Length = ";
-      print_int l;
-      print_newline ()
-    | Some f ->
-      hadamard_entropy f |> Float.to_string |> print_endline
+    let process inp =
+      let f = F2N.parse_func inp in
+      match f with
+      | None ->
+        print_endline "Not a valid boolean function";
+        print_string "Length = ";
+        print_int l;
+        print_newline ()
+      | Some f ->
+        hadamard_entropy f |> Float.to_string |> print_endline in
+    List.iter ~f:process inps
